a115405f8335595277c18b30ce81d52173c30db4
Leqsott <leqsott@gmail.com>
2019-12-25 01:21:59 +0800

    Add xml_txt Converter / TestFile

---
+++ xml_txt/txt_to_xml.py
@@ -0,0 +1,45 @@
+import os
+
+
+def getxml(targetpath, filename, targettype):
+    # infile = open(targetpath + "/" + i_filename + ".txt", "r", encoding='utf-8')
+    # relafile = open(targetpath + "/" + i_filename + targettype, "r", encoding='utf-8')
+
+    infile = open(filename + ".txt", "r", encoding='utf-8')
+    relafile = open(filename + targettype, "r", encoding='utf-8')
+    outfile = open(filename + "_output.xml", "w", encoding='utf-8')
+
+    outfile.write("<weibos>\n")
+    while True:
+        strtxt = infile.readline()   # read weibo id
+        if strtxt.strip() == "--":
+            break
+        outfile.write("\t<weibo id=\""+strtxt.strip()+"\">\n")  # write weibo id
+        outfile.write("\t\t"+infile.readline())  # read and write the content
+
+        while True: # read relations
+            strrela0 = relafile.readline()
+            if strrela0.strip().isdigit():
+                break
+            strrela1 = relafile.readline()
+            outfile.write("\t\t<employee from=\""+ strrela0.strip() +"\" name=\"" + strrela1.strip() + "\"></employee>\n")
+
+        outfile.write("\t</weibo>\n")
+
+    outfile.write("</weibos>")
+    infile.close()
+    relafile.close()
+    outfile.close()
+
+    return
+
+
+targetpath = "C:/Users/Leqsott/PycharmProjects/xmltotxt"
+targettype = ".rela"
+filelist = os.listdir(targetpath)
+for i in filelist:
+    i_fullname = os.path.splitext(i)
+    i_filename = i_fullname[0]  # 读取文件名
+    i_filetype = i_fullname[1]  # 读扩展名
+    if i_filetype==targettype:  # 如果和目标扩展名匹配
+        getxml(targetpath, i_filename, targettype)
---
+++ xml_txt/xml_to_txt.py
@@ -0,0 +1,68 @@
+import os
+
+
+def findbegin(infile):
+    while True:
+        strxml = infile.readline()
+        print(strxml)
+        print("ready to read begin")
+        i = 0
+        while i<len(strxml)-7:
+            print(strxml[i:i+8])
+            if strxml[i:i+8]=="<weibos>":
+                return True
+            i = i+1
+    return False
+
+
+def fileconvert(targetpath, i_filename, targettype):
+    infile = open(targetpath + "/" + i_filename + targettype, "r", encoding='utf-8')
+    outfile = open(i_filename + ".txt", "w", encoding='utf-8')
+
+    if not findbegin:
+        outfile.write("--")
+        infile.close()
+        outfile.close()
+        return
+
+    curid = 0
+    text = 0
+    while True:
+        strxml = infile.readline()
+        i = 0
+        while i < len(strxml):
+            if i < len(strxml) - 7 and strxml[i:i + 8] == "</weibo>":  # read weibo end
+                text = 0
+                outfile.write("\n")
+                break
+            if text == 1 and strxml[i]!=' ' and strxml[i]!='\n':
+                outfile.write(strxml[i])
+            print(strxml[i:i + 9])
+            if i < len(strxml)-8 and strxml[i:i + 9] == "</weibos>":
+                outfile.write("--")
+                infile.close()
+                outfile.close()
+                return
+            elif i < len(strxml)-9 and strxml[i:i+10] == "<weibo id=":   # read weibo begin
+                i = i + 11
+                num = 0
+                while strxml[i].isdigit():         # read weibo id num
+                    num = num*10 + int(strxml[i])
+                    i = i + 1
+                curid = num
+                text = 1
+                outfile.write(str(curid)+"\n")
+                i = i + 1                        # read">"
+            i = i + 1
+    return
+
+
+targetpath = "C:/Users/Leqsott/PycharmProjects/xmltotxt"
+targettype = ".xml"
+filelist = os.listdir(targetpath)
+for i in filelist:
+    i_fullname = os.path.splitext(i)
+    i_filename = i_fullname[0]  # 读取文件名
+    i_filetype = i_fullname[1]  # 读扩展名
+    if i_filetype==targettype:  # 如果和目标扩展名匹配
+        fileconvert(targetpath, i_filename, targettype)
